2561c9535a872ca551923031d9afafa4ecda4f52
maciej <maciej.choluj@gmail.com>
2026-04-04 19:23:35 +0200

    Initial Commit

---
+++ saper/saperApp.swift
@@ -0,0 +1,17 @@
+//
+//  saperApp.swift
+//  saper
+//
+//  Created by Maciek Choluj on 04.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct saperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
